aed77974a65ab2bb26adfd3e2b6766f2a2cf93d2
Nesl-Predator <owang235@gmail.com>
2024-07-16 16:57:51 -0700

    starting to set up training script

---
+++ mamba_ssm/fn_model.py
@@ -0,0 +1,29 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class TimeModel(nn.Module):
+    def __init__(self, input_dim, genlen, device, dtype):
+        super(TimeModel, self).__init__()
+        self.genlen = genlen
+        self.input_dim = input_dim
+        self.outputFN = nn.Sequential(
+            nn.Linear(input_dim[1]*input_dim[2], genlen, bias=False, device=device, dtype=dtype),  
+            #nn.Linear(genlen, 1, bias=False, device=device, dtype=dtype)  # Transform to [batch_size, 1]
+        )
+    
+    def forward(self, embed_out):
+        # Apply the output function
+        output = self.outputFN(embed_out.view(self.input_dim[0],self.input_dim[1]*self.input_dim[2]))
+        # Ensure the output has the shape [batch_size, genlen, 1]
+        output = output.view(self.genlen) #since batch_size is 1, just get rid of it
+        return output
+
+
+
+def fn(input_ids=input_ids):
+    embed_in = model.get_input_embeddings()(input_ids)
+    timeOut = model(embed_in).last_hidden_state
+    outputModel = TimeModel(input_dim=timeOut.shape, genlen=args.genlen, device=device, dtype=dtype)
+    return outputModel(timeOut)
--- mamba_ssm/train.py
+++ mamba_ssm/train.py
@@ -3,6 +3,11 @@
 import argparse
 import time
 import json
+from accelerate import Accelerator, DeepSpeedPlugin
+from accelerate import DistributedDataParallelKwargs
+from torch import nn, optim
+from torch.optim import lr_scheduler
+from tqdm import tqdm
 
 import torch
 import torch.nn as nn
@@ -12,9 +17,16 @@ from einops import rearrange
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
+import pandas as pd
+from TimeLLM.utils.metrics import metric
+from TimeLLM.utils.tools import del_files, EarlyStopping, adjust_learning_rate, vali, load_content
+
+import wandb 
+
 #from mamba_ssm.models.mixer_seq_simple import MambaTimeHeadModel
 from models.mixer_seq_simple import MambaTimeHeadModel, MambaLMHeadModel
-#from TimeLLM.models.TimeLLM import Model #???
+from fn_model import TimeModel
+from data_provider.data_factory import data_provider
 
 parser = argparse.ArgumentParser(description="Generation benchmarking")
 parser.add_argument("--model-name", type=str, default="state-spaces/mamba-130m")
@@ -27,8 +39,75 @@ parser.add_argument("--topp", type=float, default=1.0)
 parser.add_argument("--minp", type=float, default=0.0)
 parser.add_argument("--repetition-penalty", type=float, default=1.0)
 parser.add_argument("--batch", type=int, default=1)
-args = parser.parse_args()
 
+# data loader
+parser.add_argument('--data', type=str, required=True, default='ETTm1', help='dataset type')
+parser.add_argument('--root_path', type=str, default='./dataset', help='root path of the data file')
+parser.add_argument('--data_path', type=str, default='ETT-small/ETTh1.csv', help='data file')
+parser.add_argument('--features', type=str, default='M',
+                    help='forecasting task, options:[M, S, MS]; '
+                         'M:multivariate predict multivariate, S: univariate predict univariate, '
+                         'MS:multivariate predict univariate')
+parser.add_argument('--target', type=str, default='OT', help='target feature in S or MS task')
+parser.add_argument('--loader', type=str, default='modal', help='dataset type')
+parser.add_argument('--freq', type=str, default='h',
+                    help='freq for time features encoding, '
+                         'options:[s:secondly, t:minutely, h:hourly, d:daily, b:business days, w:weekly, m:monthly], '
+                         'you can also use more detailed freq like 15min or 3h')
+parser.add_argument('--checkpoints', type=str, default='./checkpoints/', help='location of model checkpoints')
+
+# forecasting task
+parser.add_argument('--seq_len', type=int, default=96, help='input sequence length')
+parser.add_argument('--label_len', type=int, default=48, help='start token length')
+parser.add_argument('--pred_len', type=int, default=96, help='prediction sequence length')
+parser.add_argument('--seasonal_patterns', type=str, default='Monthly', help='subset for M4')
+
+# model define
+parser.add_argument('--enc_in', type=int, default=7, help='encoder input size')
+parser.add_argument('--dec_in', type=int, default=7, help='decoder input size')
+parser.add_argument('--c_out', type=int, default=7, help='output size')
+parser.add_argument('--d_model', type=int, default=16, help='dimension of model')
+parser.add_argument('--n_heads', type=int, default=8, help='num of heads')
+parser.add_argument('--e_layers', type=int, default=2, help='num of encoder layers')
+parser.add_argument('--d_layers', type=int, default=1, help='num of decoder layers')
+parser.add_argument('--d_ff', type=int, default=32, help='dimension of fcn')
+parser.add_argument('--moving_avg', type=int, default=25, help='window size of moving average')
+parser.add_argument('--factor', type=int, default=1, help='attn factor')
+parser.add_argument('--dropout', type=float, default=0.1, help='dropout')
+parser.add_argument('--embed', type=str, default='timeF',
+                    help='time features encoding, options:[timeF, fixed, learned]')
+parser.add_argument('--activation', type=str, default='gelu', help='activation')
+parser.add_argument('--output_attention', action='store_true', help='whether to output attention in encoder')
+parser.add_argument('--patch_len', type=int, default=16, help='patch length')
+parser.add_argument('--stride', type=int, default=8, help='stride')
+parser.add_argument('--prompt_domain', type=int, default=0, help='')
+parser.add_argument('--llm_model', type=str, default='Mamba', help='LLM model') # LLAMA, GPT2, BERT, Mamba
+parser.add_argument('--llm_dim', type=int, default='2560', help='LLM model dimension')#Mamba:768 LLama7b:4096; GPT2-small:768; BERT-base:768
+parser.add_argument('--num_params', type=str, default='130m', help='string of our param size to append to huggingface')
+
+# optimization
+parser.add_argument('--num_workers', type=int, default=10, help='data loader num workers')
+parser.add_argument('--itr', type=int, default=1, help='experiments times')
+parser.add_argument('--train_epochs', type=int, default=10, help='train epochs')
+parser.add_argument('--align_epochs', type=int, default=10, help='alignment epochs')
+parser.add_argument('--batch_size', type=int, default=32, help='batch size of train input data')
+parser.add_argument('--eval_batch_size', type=int, default=8, help='batch size of model evaluation')
+parser.add_argument('--patience', type=int, default=10, help='early stopping patience')
+parser.add_argument('--learning_rate', type=float, default=0.0001, help='optimizer learning rate')
+parser.add_argument('--des', type=str, default='test', help='exp description')
+parser.add_argument('--loss', type=str, default='MSE', help='loss function')
+parser.add_argument('--lradj', type=str, default='type1', help='adjust learning rate')
+parser.add_argument('--pct_start', type=float, default=0.2, help='pct_start')
+parser.add_argument('--use_amp', action='store_true', help='use automatic mixed precision training', default=False)
+parser.add_argument('--llm_layers', type=int, default=6)
+parser.add_argument('--percent', type=int, default=100)
+
+args = parser.parse_args()
+path = args.model_comment
+args.content = load_content(args)
+if not os.path.exists(path) and accelerator.is_local_main_process:
+    os.makedirs(path)
+    
 repeats = 10
 device = "cuda"
 dtype = torch.float16
@@ -48,8 +127,13 @@ print(f"Number of parameters: {sum(p.numel() for p in model.parameters() if p.re
 #training time! we need some data and stuff
 model.train()
 
+#need to change these args...
 
+train_data, train_loader = data_provider(args, 'train')
+vali_data, vali_loader = data_provider(args, 'val')
+test_data, test_loader = data_provider(args, 'test')
 
+print("test data! ", test_data)
 #this is eval mode, let's train before here
 model.eval()
 torch.random.manual_seed(0)
@@ -57,88 +141,29 @@ torch.random.manual_seed(0)
 if args.prompt is None:
     input_ids = torch.randint(1, 1000, (args.batch, args.promptlen), dtype=torch.long, device="cuda")
     attn_mask = torch.ones_like(input_ids, dtype=torch.long, device="cuda")
+    #args.prompt = test_data
+    
 else:
     
     tokens = tokenizer(args.prompt, return_tensors="pt")
     input_ids = tokens.input_ids.to(device=device)
     attn_mask = tokens.attention_mask.to(device=device)
-    
-
-max_length = input_ids.shape[1] + args.genlen
 
 
+input_ids = torch.tensor(list(map(float, args.prompt.split(','))),device=device,dtype=int).unsqueeze(0)
 
-'''
-if is_mamba:
-    fn = lambda: model.generate(
-        input_ids=input_ids,
-        max_length=max_length,
-        cg=True,
-        return_dict_in_generate=True,
-        output_scores=True,
-        enable_timing=False,
-        temperature=args.temperature,
-        top_k=args.topk,
-        top_p=args.topp,
-        min_p=args.minp,
-        repetition_penalty=args.repetition_penalty,
-    )
-else:
-    fn = lambda: model.generate(
-        input_ids=input_ids,
-        attention_mask=attn_mask,
-        max_length=max_length,
-        return_dict_in_generate=True,
-        pad_token_id=tokenizer.eos_token_id,
-        do_sample=True,
-        temperature=args.temperature,
-        top_k=args.topk,
-        top_p=args.topp,
-        repetition_penalty=args.repetition_penalty,
-    )
-'''
-class TimeModel(nn.Module):
-    def __init__(self, input_dim, genlen, device, dtype):
-        super(TimeModel, self).__init__()
-        self.genlen = genlen
-        self.input_dim = input_dim
-        self.outputFN = nn.Sequential(
-            nn.Linear(input_dim[1]*input_dim[2], genlen, bias=False, device=device, dtype=dtype),  
-            #nn.Linear(genlen, 1, bias=False, device=device, dtype=dtype)  # Transform to [batch_size, 1]
-        )
-    
-    def forward(self, embed_out):
-        # Apply the output function
-        output = self.outputFN(embed_out.view(self.input_dim[0],self.input_dim[1]*self.input_dim[2]))
-        # Ensure the output has the shape [batch_size, genlen, 1]
-        output = output.view(self.genlen) #since batch_size is 1, just get rid of it
-        return output
+#print("input ids: ", input_ids)
+#max_length = input_ids.shape[1] + args.genlen
 
-embed_in = model.get_input_embeddings()(input_ids)
-
-def fn():
+def fn(input_ids=input_ids):
+    embed_in = model.get_input_embeddings()(input_ids)
     timeOut = model(embed_in).last_hidden_state
     outputModel = TimeModel(input_dim=timeOut.shape, genlen=args.genlen, device=device, dtype=dtype)
     return outputModel(timeOut)
 
 
 out = fn()
-'''
-#THINGS TO TEMPORARILY DEFINE
-d_ff = 128
-n_vars = args.genlen #???
 
-dec_out = out[:, :, :d_ff]
-
-dec_out = torch.reshape(
-    dec_out, (-1, n_vars, dec_out.shape[-2], dec_out.shape[-1]))
-dec_out = dec_out.permute(0, 1, 3, 2).contiguous()
-
-dec_out = self.output_projection(dec_out[:, :, :, -self.patch_nums:])
-dec_out = dec_out.permute(0, 2, 1).contiguous()
-
-dec_out = self.normalize_layers(dec_out, 'denorm')
-'''
 
 if args.prompt is not None:
     #print(tokenizer.batch_decode(out.sequences.tolist()))
