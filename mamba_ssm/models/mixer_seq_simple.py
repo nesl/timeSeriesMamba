66e21dbdcf2ef4ec3def4b0ce0447b0634294524
Nesl-Predator <owang235@gmail.com>
2024-07-16 14:39:01 -0700

    timeMamba generation

--- mamba_ssm/models/mixer_seq_simple.py
+++ mamba_ssm/models/mixer_seq_simple.py
@@ -332,24 +332,25 @@ class MambaTimeHeadModel(nn.Module, GenerationMixin):
         d_model = config.d_model
         n_layer = config.n_layer
         d_intermediate = config.d_intermediate
-        vocab_size = config.vocab_size
+        #vocab_size = config.vocab_size
+        output_size = config.vocab_size
         ssm_cfg = config.ssm_cfg
         attn_layer_idx = config.attn_layer_idx
         attn_cfg = config.attn_cfg
         rms_norm = config.rms_norm
         residual_in_fp32 = config.residual_in_fp32
         fused_add_norm = config.fused_add_norm
-        pad_vocab_size_multiple = config.pad_vocab_size_multiple
+        #pad_vocab_size_multiple = config.pad_vocab_size_multiple
         factory_kwargs = {"device": device, "dtype": dtype}
 
         super().__init__()
-        if vocab_size % pad_vocab_size_multiple != 0:
-            vocab_size += pad_vocab_size_multiple - (vocab_size % pad_vocab_size_multiple)
+        #if vocab_size % pad_vocab_size_multiple != 0:
+        #    vocab_size += pad_vocab_size_multiple - (vocab_size % pad_vocab_size_multiple)
         self.backbone = MixerModel(
             d_model=d_model,
             n_layer=n_layer,
             d_intermediate=d_intermediate,
-            vocab_size=vocab_size,
+            vocab_size=output_size,
             ssm_cfg=ssm_cfg,
             attn_layer_idx=attn_layer_idx,
             attn_cfg=attn_cfg,
@@ -359,7 +360,7 @@ class MambaTimeHeadModel(nn.Module, GenerationMixin):
             residual_in_fp32=residual_in_fp32,
             **factory_kwargs,
         )
-        self.time_head = nn.Linear(d_model, vocab_size, bias=False, **factory_kwargs)
+        self.time_head = nn.Linear(d_model, output_size, bias=False, **factory_kwargs)
 
         # Initialize weights and apply final processing
         self.apply(
@@ -378,17 +379,19 @@ class MambaTimeHeadModel(nn.Module, GenerationMixin):
     def allocate_inference_cache(self, batch_size, max_seqlen, dtype=None, **kwargs):
         return self.backbone.allocate_inference_cache(batch_size, max_seqlen, dtype=dtype, **kwargs)
 
-    def forward(self, input_ids, position_ids=None, inference_params=None, num_last_tokens=0, **mixer_kwargs):
+    def forward(self, input_embeddings, position_ids=None, inference_params=None, num_last_tokens=0, **mixer_kwargs):
         """
         "position_ids" is just to be compatible with Transformer generation. We don't use it.
         num_last_tokens: if > 0, only return the logits for the last n tokens
         """
-        hidden_states = self.backbone(input_ids, inference_params=inference_params, **mixer_kwargs)
+        hidden_states = self.backbone(input_embeddings, inference_params=inference_params, **mixer_kwargs)
         if num_last_tokens > 0:
             hidden_states = hidden_states[:, -num_last_tokens:]
         time_logits = self.time_head(hidden_states)
-        CausalTimeOutput = namedtuple("CausalTimeOutput", ["logits"])
-        return CausalTimeOutput(logits=time_logits)
+        CausalTimeOutput = namedtuple("CausalTimeOutput", ["logits","last_hidden_state"])
+        #print("hidden states: ", hidden_states)
+        #print("hidden_states[-1]", hidden_states[-1])
+        return CausalTimeOutput(logits=time_logits,last_hidden_state=hidden_states)
 
     #maybe we can start with the pretrained version, but can still train it to behave somewhat differently?
     @classmethod
@@ -425,3 +428,6 @@ class MambaTimeHeadModel(nn.Module, GenerationMixin):
         config_path = os.path.join(save_directory, 'config.json')
         with open(config_path, 'w') as f:
             json.dump(self.config.__dict__, f, indent=4)
+    
+    def get_input_embeddings(self):
+        return self.backbone.embedding
