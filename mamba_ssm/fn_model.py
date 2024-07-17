380f423d8e2969377be2905eb8a9bce5bccf481a
Nesl-Predator <owang235@gmail.com>
2024-07-16 17:05:40 -0700

    setup for training

--- mamba_ssm/fn_model.py
+++ mamba_ssm/fn_model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class TimeModel(nn.Module):
-    def __init__(self, input_dim, genlen, device, dtype):
+    def __init__(self, model, input_dim, genlen, device, dtype):
         super(TimeModel, self).__init__()
         self.genlen = genlen
         self.input_dim = input_dim
@@ -20,10 +20,11 @@ class TimeModel(nn.Module):
         output = output.view(self.genlen) #since batch_size is 1, just get rid of it
         return output
 
+'''
 
-
-def fn(input_ids=input_ids):
+def fn(input_ids):
     embed_in = model.get_input_embeddings()(input_ids)
     timeOut = model(embed_in).last_hidden_state
     outputModel = TimeModel(input_dim=timeOut.shape, genlen=args.genlen, device=device, dtype=dtype)
     return outputModel(timeOut)
+'''
